d4e43b05ea57bfa7e9453f9f380e3fbbe5509ba1
Mateo Pavoni <mateopavoni905@gmail.com>
2025-10-31 22:20:09 -0300

    feat: agrega componente ItemList para renderizar lista de productos

---
+++ src/componentes/ItemList.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Item from './Item';
+
+const ItemList = ({ productos }) => {
+    return (
+        <div
+        style={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            justifyContent: 'center',
+            gap: '20px',
+        }}
+        >
+        {productos.map((producto) => (
+            <Item key={producto.id} producto={producto} />
+        ))}
+        </div>
+    );
+};
+
+export default ItemList;
